f1d5852be8cbf94bb3e0fa72eb2c32c0a3008a90
Akinwande Gbenga Vincent <akinwandegbengavincent@gmail.com>
2022-04-14 18:37:34 +0100

    Added all buttons for processed data

--- frontend/src/assets/js/index.js
+++ frontend/src/assets/js/index.js
@@ -15,78 +15,39 @@ const dataType = [
               header: false,
             },
             {
-              name: "Encoded Data",
-              path: "../details/csv_files/email.csv",
+              name: "Feature To Category",
+              path: "../../backend/precrossing_data_ouput/maxwell/dataset/convert_to_cat_features.csv",
+              header: false,
+            },
+            {
+              name: "Replace Missing With 'Nan'",
+              path: "../../backend/precrossing_data_ouput/maxwell/dataset/fill_out_missing_for_cat2.csv",
+              header: false,
+            },
+            {
+              name: "Replace 'Nan' With Values (MICE)",
+              path: "../../backend/precrossing_data_ouput/maxwell/dataset/after_mice_imputer.csv",
+              header: false,
+            },
+            {
+              name: "Round Off",
+              path: "../../backend/precrossing_data_ouput/maxwell/dataset/after_round_of_values.csv",
+              header: true,
+            },
+            {
+              name: "One Hot Encoded Data",
+              path: "../../backend/precrossing_data_ouput/maxwell/dataset/after_one_hot_encoding.csv",
               header: false,
             },
             {
               name: "Scaled Data",
-              path: "../details/csv_files/email.csv",
-              header: false,
-            },
-            {
-              name: "Transformed Data",
-              path: "../details/csv_files/email.csv",
-              header: false,
+              path: "../../backend/precrossing_data_ouput/maxwell/dataset/maxwell_standardization.csv",
+              header: true,
             }
           ],
         },
         {
-          name: "Preprocessing Result",
-          path: "../../backend/precrossing_data_ouput/maxwell/dataset/maxwell_raw.csv",
-          header: true,
-          buttons: [
-            {
-              name: "Raw Data",
-              path: "../../backend/precrossing_data_ouput/maxwell/dataset/maxwell_raw.csv",
-              header: false,
-            },
-            {
-              name: "Encoded Data",
-              path: "../details/csv_files/email.csv",
-              header: false,
-            },
-            {
-              name: "Scaled Data",
-              path: "../details/csv_files/email.csv",
-              header: false,
-            },
-            {
-              name: "Transformed Data",
-              path: "../details/csv_files/email.csv",
-              header: false,
-            }
-          ],
-        },
-        {
-          name: "Preprocessing Result",
-          path: "../../backend/precrossing_data_ouput/maxwell/dataset/maxwell_raw.csv",
-          header: true,
-          buttons: [
-            {
-              name: "Raw Data",
-              path: "../../backend/precrossing_data_ouput/maxwell/dataset/maxwell_raw.csv",
-              header: false,
-            },
-            {
-              name: "Encoded Data",
-              path: "../details/csv_files/email.csv",
-              header: false,
-            },
-            {
-              name: "Scaled Data",
-              path: "../details/csv_files/email.csv",
-              header: false,
-            },
-            {
-              name: "Transformed Data",
-              path: "../details/csv_files/email.csv",
-              header: false,
-            }
-          ],
-        },
-        {
-          name: "Maxwell Without Kmeans testing length",
+          name: "Maxwell With Kmeans",
           path: "../details/csv_files/email.csv",
           header: true,
           buttons: [
@@ -107,14 +68,34 @@ const dataType = [
     listInner: {
       list: [
         {
-          name: "Desh With Kmeans",
-          path: "../details/csv_files/email.csv",
+          name: "Preprocessing Result",
+          path: "../../backend/precrossing_data_ouput/desh/dataset/desh_raw.csv",
           header: true,
           buttons: [
             {
               name: "Raw Data",
+              path: "../../backend/precrossing_data_ouput/desh/dataset/desh_raw.csv",
               header: false,
-              path: "../details/csv_files/email.csv",
+            },
+            {
+              name: "Replace Missing With 'Nan'",
+              path: "../../backend/precrossing_data_ouput/desh/dataset/fill_missing_with_nan.csv",
+              header: false,
+            },
+            {
+              name: "Replace 'Nan' With Values (MICE)",
+              path: "../../backend/precrossing_data_ouput/desh/dataset/fill_nan_with_mice_imputer.csv",
+              header: false,
+            },
+            {
+              name: "Round Off",
+              path: "../../backend/precrossing_data_ouput/desh/dataset/after_round_off_value.csv",
+              header: true,
+            },
+            {
+              name: "Scaled Data",
+              path: "../../backend/precrossing_data_ouput/desh/dataset/desh_standardizatino.csv",
+              header: true,
             }
             
           ],
